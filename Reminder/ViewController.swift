e01ef7d9c3978df5a05a9c83c582ebfc92b1864b
AisultanAskarov <36818367+AisultanAskarov@users.noreply.github.com>
2022-10-28 02:03:34 +0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Aisultan Askarov on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
